013d399f2e330ebbbe19b5a8e877c0971415dbae
piyawat pintusornsri <piyawat@softever.co.th>
2024-01-17 15:28:01 +0700

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/piyawat5/myproject
+
+go 1.21.5
---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/piyawat5/cinema/movie"
+)
+
+func main() {
+	fmt.Println("test")
+	movie.Review()
+}
